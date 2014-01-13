7d3522bd0a903b396ca3b247072a1985e6842e31
Piotr <Piotr@Piotr-PC.home>
2014-01-13 23:13:03 +0100

    Adds LaunchWindow functionnality

--- RevolutionnaryBot/src/revolutionnarybot/RevolutionnaryBot.java
+++ RevolutionnaryBot/src/revolutionnarybot/RevolutionnaryBot.java
@@ -16,7 +16,7 @@ public class RevolutionnaryBot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LaunchWindow w = new LaunchWindow();
     }
     
 }
